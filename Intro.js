475868a5f7ddc79c49a8a3949342d508ebcb7975
Kayode06 <adenibuyankayode@gmail.com>
2026-09-12 17:49:08 +0100

    Create Intro.js

---
+++ Intro.js
@@ -0,0 +1,10 @@
+
+const name = "Kayode";
+const age = 21;
+const favoriteProgrammingLanguage = "Python";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favoriteProgrammingLanguage}`);
+
+console.log(`Hello! My name is ${name}, I am ${age} years old, and my favorite programming language is ${favoriteProgrammingLanguage}.`);
